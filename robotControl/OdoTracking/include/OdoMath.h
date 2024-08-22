f81215d88c99d62bfe70f547ca1407fb8b2a0ec9
MobMasher21 <70303582+MobMasher21@users.noreply.github.com>
2024-08-21 20:14:19 -0400

    start on odo

--- robotControl/OdoTracking/include/OdoMath.h
+++ robotControl/OdoTracking/include/OdoMath.h
@@ -9,16 +9,9 @@ namespace evAPI {
 
 class OdoMath {
   public:
-    void setPosition(int xPos, int yPos);                                                              // sets the current robot position
-    void resetHeading();                                                                               // resets the stored heading to 0
-    void runMath(int travelDistance, int robotHeading, int headingChange, leftAndRight arcDirection);  // runs the odo math and updates the counters
-    int getXPosition();                                                                                // returns the robot x position
-    int getYPosition();                                                                                // returns the robot y position
-
+  
   private:
-    int xPosition;        // coordinate of the robot x
-    int yPosition;        // coordinate of the robot y
-    int previousHeading;  // heading last time the math was run
+  
 };
 
 }  // namespace evAPI
